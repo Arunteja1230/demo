58d51d55ac2c50ab0fb45414e67db1d27a39ba2b
Arun Teja Arikatla <aa21010@corp.ojas-it.com>
2023-05-16 15:36:16 +0530

    first code

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    value:0
+}
+
+
+const createSlice=createSlice({
+    name:"counter",
+    initialState,
+    reducers:{
+        incer:()=>{
+
+        }
+    },
+})
+export const {incer}=createSlice.actions
+
+export default createSlice.reducers
